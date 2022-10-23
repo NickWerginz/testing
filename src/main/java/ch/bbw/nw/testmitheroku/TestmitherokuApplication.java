3c99bebc99ea1a18804d219be48042598c8e5a65
Nick Werginz <nick.werginz@lernende.bbw.ch>
2022-10-23 12:31:03 +0200

    first commit

---
+++ src/main/java/ch/bbw/nw/testmitheroku/TestmitherokuApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.nw.testmitheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestmitherokuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestmitherokuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/bbw/nw/testmitheroku/TestmitherokuApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.nw.testmitheroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestmitherokuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
